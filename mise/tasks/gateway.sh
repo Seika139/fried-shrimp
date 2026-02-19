bfa4947464cb31dba46eea57a182e5db4704d29e
Seika139 <tcoanzu@gmail.com>
2026-02-19 04:26:11 +0000

    dotenvx を排除

--- mise/tasks/gateway.sh
+++ mise/tasks/gateway.sh
@@ -3,9 +3,8 @@
 #MISE description="ゲートウェイを起動する"
 #MISE shell="bash -c"
 #MISE quiet=true
-#MISE depends=["dotenvx"]
 
 SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 source "${SCRIPT_DIR}/../common.sh"
 
-dotenvx run -- pnpm gateway:dev
+pnpm gateway:dev
